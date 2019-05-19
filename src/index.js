0e3eb2d8807e1d66168270093f561dbc6eb3b48e
mdons <michaeljdonnell@gmail.com>
2019-05-19 14:26:26 -0500

    checkers2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import {Game} from './Game.js';
+
+// ========================================
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
